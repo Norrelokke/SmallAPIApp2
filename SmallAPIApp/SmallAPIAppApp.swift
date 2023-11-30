67ced45a5835a7cb1c4f887c6d6203a3f443df35
MikaelaNorrelokke <M.Norrelokke@outlook.com>
2023-11-30 15:31:19 +0100

    Initial Commit

---
+++ SmallAPIApp/SmallAPIAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SmallAPIAppApp.swift
+//  SmallAPIApp
+//
+//  Created by Mikaela Nörrelökke on 2023-11-30.
+//
+
+import SwiftUI
+
+@main
+struct SmallAPIAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
